2fe83d1a6c2f83eabaad710b077a083f08c99895
Edo777 <edgar.khachatryan95@mail.com>
2017-05-18 19:11:39 +0400

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 
-function speak(){
-    console.log('hello world');
+const speak = function(){
+    const arr = ['foo', 'bar', 'baz'];
+    for(const i of arr){
+        console.log(i.toUpperCase());
+    }
 }
 speak();

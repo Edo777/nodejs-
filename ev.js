3361f7b7b5f8b4c8069cb770ff1d14bd5e0d55f7
Edo777 <edgar.khachatryan95@mail.com>
2017-05-18 20:43:53 +0400

    ev.process

--- ev.js
+++ ev.js
@@ -1,3 +1,10 @@
 'use strict'
+/*
 let counter = 0;
 let k = setInterval(function(){console.log(`Called ${++counter} times`); if(counter === 6){clearInterval(k);}}, 1000);
+*/
+
+process.stdin.on('data', function(d){
+    console.log(d);
+        console.log(d.toString().toUpperCase());
+})

7111222837a47689ee268f1dfb69742a152aff1d
Edo777 <edgar.khachatryan95@mail.com>
2017-05-19 18:39:55 +0400

    index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const speak = function(arr){
     for(const i of arr){
         newArray.push(i.toUpperCase());
     }
-    console.log(i);
     return newArray;
 }
 const res = speak(['a', 'b', 'c']);

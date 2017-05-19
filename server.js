a950f82eb86c5e59258fb078f9e416d6789913f1
Edo777 <edgar.khachatryan95@mail.com>
2017-05-19 18:42:03 +0400

    server.js

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 'use strict';
 
 const http = require('http');
+const p = require('./ports');
 const server = http.createServer( (req,res)=>{
     if(req.url ==='/'){
         res.setHeader('content-type','text/html');
         res.end(`
             <!doctype html>
             <body>
-                <h1> some header!</h1>
+                <h1> ${p.upper(__dirname)}</h1>
                 <p>What a body =P</p>
             </body>
         `)

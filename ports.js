55ea9800470f08dc4a92b56edf2eef05fcdce69b
Edo777 <edgar.khachatryan95@mail.com>
2017-05-19 18:50:33 +0400

    new sintax

--- ports.js
+++ ports.js
@@ -3,7 +3,11 @@
 const upper = (str) => {
     return str.toUpperCase();
 }
+const low = (str) => {
+    return str.toLowerCase();
+}
 //Fat arrow function , lambda function
 module.exports = {
-    upper
+    one : upper,
+    two : low
 }

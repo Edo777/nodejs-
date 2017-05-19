10c379db62d7fd4a759ce25b4d3e03c220dc5a7d
Edo777 <edgar.khachatryan95@mail.com>
2017-05-19 21:21:03 +0400

    Class

---
+++ ClassWork.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const retVal = (myList)=>{
+    const data = myList.reduce((prev,total) => prev + total);
+    const headerpart = data.split('\r\n\r\n')[0];
+    const bodypart = data.split('\r\n\r\n')[1];
+    const retObj = {};
+    const headArray = [];
+    const bodyArray = [];
+    for(const i of headerpart.split('\r\n')){
+        headArray.push(i);
+    }
+    for(const i of bodypart.split('\r\n')){
+        bodyArray.push(i);
+    }
+    retObj['head'] = headArray;
+    retObj['body'] = bodyArray;
+    return retObj;
+}
+    module.exports = {
+        retVal
+    }
+}

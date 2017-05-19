bb68fa2603ffa3eb49a9f74e7e554440794e3096
Edo777 <edgar.khachatryan95@mail.com>
2017-05-19 21:20:34 +0400

    socket

--- socket.js
+++ socket.js
@@ -3,9 +3,11 @@
 const net = require('net');
 const sock = new net.Socket;
 
+
+
 sock.on('connect', () => {
-    sock.write('Get /en/HTTP/1.1\r\n');
-    sock.write('Host:istc.am');
+    sock.write('GET /en/HTTP/1.1 \r\n');
+    sock.write('Host : istc.am');
     sock.write('\r\n\r\n');
 });
 sock.setEncoding('utf-8');
@@ -14,7 +16,10 @@ const total_data = [];
 
 sock.on('data', d => total_data.push(d));
 
-sock.on('end', () => console.log(total_data.reduce((prev, total)=> prev + total)));
+sock.on('end',
+
+    () => console.log(total_data.reduce((prev, total)=> prev + total)));
+
 sock.connect({
     port: 80,
     host: 'istc.am'
